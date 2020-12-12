9a0406fe7191d7e10ecf1690f38e86613e6b775f
Ben McRae <benmcrae5@yahoo.com>
2020-12-11 19:34:11 -0700

    functional server...

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,16 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
+
+const PORT = 3003;
+
+
+app.get("/weird", (req, res) => {
+    res.send("This is getting through!");
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server good to go!");
+});
+
