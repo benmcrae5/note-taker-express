9a6238f9f3f915f6084e674fcaca64934e5b0179
Ben McRae <benmcrae5@yahoo.com>
2020-12-12 17:06:27 -0700

    added delete functionality

--- Develop/server.js
+++ Develop/server.js
@@ -22,8 +22,9 @@ app.get("/api/notes", (req, res) => {
     res.json(noteData);
 });
 
+//good?
 app.post("/api/notes", (req, res) => {
-    req.body.ID = uuidv4();
+    req.body.id = uuidv4();
     let newNote = noteData;
     newNote.push(req.body);
 
@@ -35,9 +36,24 @@ app.post("/api/notes", (req, res) => {
     res.json(newNote);
 });
 
+app.delete("/api/notes/:identifier", (req, res) => {
+    let index = noteData.findIndex(el => el.id === req.params.identifier);
+    console.log(index);
+    console.log(noteData[index]);
+    let updatedArray = noteData;
+    updatedArray.splice(index, 1);
+    fs.writeFileSync(
+        path.join(__dirname + "/db/db.json"), 
+        JSON.stringify(updatedArray)
+    );
+    
+    res.json(updatedArray);
+})
+
 //default to main page GOES AT END OF APP CALLS
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
+    
 });
 
 //good
